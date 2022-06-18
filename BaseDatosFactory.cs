52ccab3968c6ecbb1c777bc720aeced7e5204212
melendez <melenava@gmail.com>
2022-06-18 00:19:22 -0600

    Carga de utilidad factory Base Datos

---
+++ BaseDatosFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SUN.FactoryDB
+{
+    public class BaseDatosFactory
+    {
+        private static string TipoDB = System.Configuration.ConfigurationManager.AppSettings["TipoDB"];
+        public static AdaptadorBD obtenerConector()
+        {
+
+            TiposBaseDatos tipo;
+            Enum.TryParse<TiposBaseDatos>(TipoDB, out tipo);
+
+            switch (tipo)
+            {
+                case TiposBaseDatos.SQLServer:
+                    return new ConectorSQLServer();
+                case TiposBaseDatos.Oracle:
+                    return new ConectorOracle();
+                default:
+                    throw new Exception("No soportado");
+            }
+        }
+
+        public static AdaptadorBD obtenerConector(string servidor, string baseDatos, string usuario, string password, string esquema)
+        {
+
+            TiposBaseDatos tipo;
+            Enum.TryParse<TiposBaseDatos>(TipoDB, out tipo);
+
+            switch (tipo)
+            {
+                case TiposBaseDatos.SQLServer:
+                    return new ConectorSQLServer(servidor,baseDatos,usuario,password);
+                case TiposBaseDatos.Oracle:
+                    return new ConectorOracle(servidor, baseDatos, usuario, password, esquema);
+                default:
+                    throw new Exception("No soportado");
+            }
+        }
+    }
+}
