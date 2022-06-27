e950eb789cd94e67080f7ed7fec6d35bbb75ad69
melendez <melenava@gmail.com>
2022-06-27 13:51:07 -0600

    Enumeradores para Factory de base de datos

---
+++ FactoryEnum.cs
@@ -0,0 +1,21 @@
+
+
+namespace SUN.FactoryDB
+{
+    public enum  TiposBaseDatos
+    {
+        SQLServer,
+        Oracle,
+		MySQL
+    }
+
+    public enum TiposDatos
+    {
+        Int,
+        String,
+        DateTime,
+        Decimal,
+        Cursor,
+        Clob
+    }
+}
